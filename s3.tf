1f85ca6a57359e0bebb3b7e599f4d866a0d78f1f
hikarunakatani <hikamichael@hotmail.co.jp>
2024-04-24 17:52:27 +0900

    modified some settings #2

--- s3.tf
+++ s3.tf
@@ -26,3 +26,10 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   policy = data.aws_iam_policy_document.bucket_policy.json
 }
 
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.dataset.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
